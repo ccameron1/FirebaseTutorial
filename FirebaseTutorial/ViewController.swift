5216200b0f0895b0578f9c448bca6dce35aeb017
Carly Cameron <cmcameron@unomaha.edu>
2020-07-15 18:42:58 -0500

    dev

--- FirebaseTutorial/ViewController.swift
+++ FirebaseTutorial/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onGoPressed(_ sender: Any) {
+    }
+    
 }
 
