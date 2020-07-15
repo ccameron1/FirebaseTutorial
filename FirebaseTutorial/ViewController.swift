f9206f236c12e680c4a9fb93105e04b3460f6442
Carly Cameron <cmcameron@unomaha.edu>
2020-07-15 18:37:24 -0500

    Initial Commit

---
+++ FirebaseTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseTutorial
+//
+//  Created by Carly Cameron on 7/15/20.
+//  Copyright © 2020 Carly Cameron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
